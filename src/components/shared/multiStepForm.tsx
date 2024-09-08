561a54bc75b208931aea5fac40ca3a08070f9a75
NeilRow56 <neilrowland56@gmail.com>
2024-09-08 09:18:48 +0100

    MultiStepForm upto console.log data

--- src/components/shared/multiStepForm.tsx
+++ src/components/shared/multiStepForm.tsx
@@ -4,50 +4,75 @@ import React, { useState, useTransition } from "react";
 import { motion } from "framer-motion";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { FormDataSchema } from "@/schemas/formData";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { ArrowLeft, Atom, CalendarIcon, Loader2, LogIn } from "lucide-react";
+type Inputs = z.infer<typeof FormDataSchema>;
 
-import Link from "next/link";
+// Form Data
+
+const steps = [
+  {
+    id: "Step 1",
+    name: "Personal Information",
+    fields: ["firstName", "lastName", "email"],
+  },
+  {
+    id: "Step 2",
+    name: "Address",
+    fields: ["country", "state", "city", "street", "zip"],
+  },
+  { id: "Step 3", name: "Complete" },
+];
+// End of Form Data
 
 export default function MultiStepForm() {
   const [previousStep, setPreviousStep] = useState(0);
   const [currentStep, setCurrentStep] = useState(0);
   const delta = currentStep - previousStep;
 
-  const steps = [
-    {
-      id: "Step 1",
-      name: "Personal Information",
-      fields: ["firstName", "lastName", "email"],
-    },
-    {
-      id: "Step 2",
-      name: "Address",
-      fields: ["country", "state", "city", "street", "zip"],
-    },
-    { id: "Step 3", name: "Complete" },
-  ];
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    trigger,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(FormDataSchema),
+  });
 
-  const next = () => {
+  const processForm: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  type FieldName = keyof Inputs;
+
+  //Change step
+  const next = async () => {
+    const fields = steps[currentStep].fields;
+    const output = await trigger(fields as FieldName[], { shouldFocus: true });
+
+    if (!output) return;
+
     if (currentStep < steps.length - 1) {
+      if (currentStep === steps.length - 2) {
+        await handleSubmit(processForm)();
+      }
+      setPreviousStep(currentStep);
       setCurrentStep((step) => step + 1);
     }
   };
 
   const prev = () => {
     if (currentStep > 0) {
+      setPreviousStep(currentStep);
       setCurrentStep((step) => step - 1);
     }
   };
+
+  // End of change step
 
   return (
     <section className="container inset-3 flex flex-col justify-between p-24">
@@ -87,7 +112,7 @@ export default function MultiStepForm() {
       </nav>
 
       {/* Form */}
-      <form className="mt-12 py-12" onSubmit={() => {}}>
+      <form className="mt-12 py-12" onSubmit={handleSubmit(processForm)}>
         {currentStep === 0 && (
           <motion.div
             initial={{ x: delta >= 0 ? "50%" : "-50%", opacity: 0 }}
@@ -104,7 +129,7 @@ export default function MultiStepForm() {
               <div className="sm:col-span-3">
                 <label
                   htmlFor="firstName"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   First name
                 </label>
@@ -112,16 +137,22 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="firstName"
+                    {...register("firstName")}
                     autoComplete="given-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.firstName?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.firstName.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="sm:col-span-3">
                 <label
                   htmlFor="lastName"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   Last name
                 </label>
@@ -129,16 +160,22 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="lastName"
+                    {...register("lastName")}
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.lastName?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.lastName.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
-              <div className="sm:col-span-4">
+              <div className="sm:col-span-3">
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   Email address
                 </label>
@@ -146,9 +183,15 @@ export default function MultiStepForm() {
                   <input
                     id="email"
                     type="email"
+                    {...register("email")}
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.email?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
@@ -164,7 +207,7 @@ export default function MultiStepForm() {
             <h2 className="text-base font-semibold leading-7 text-gray-900">
               Address
             </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
+            <p className="mt-1 px-2 text-sm leading-6 text-gray-600">
               Address where you can receive mail.
             </p>
 
@@ -172,27 +215,33 @@ export default function MultiStepForm() {
               <div className="sm:col-span-3">
                 <label
                   htmlFor="country"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   Country
                 </label>
                 <div className="mt-2">
                   <select
                     id="country"
+                    {...register("country")}
                     autoComplete="country-name"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
                     <option>United States</option>
                     <option>Canada</option>
                     <option>Mexico</option>
                   </select>
+                  {errors.country?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.country.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="col-span-full">
                 <label
                   htmlFor="street"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   Street address
                 </label>
@@ -200,16 +249,22 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="street"
+                    {...register("street")}
                     autoComplete="street-address"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.street?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.street.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="sm:col-span-2 sm:col-start-1">
                 <label
                   htmlFor="city"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   City
                 </label>
@@ -217,16 +272,22 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="city"
+                    {...register("city")}
                     autoComplete="address-level2"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.city?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.city.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="sm:col-span-2">
                 <label
                   htmlFor="state"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   State / Province
                 </label>
@@ -234,16 +295,22 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="state"
+                    {...register("state")}
                     autoComplete="address-level1"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.state?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.state.message}
+                    </p>
+                  )}
                 </div>
               </div>
 
               <div className="sm:col-span-2">
                 <label
                   htmlFor="zip"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block px-2 text-sm font-medium leading-6 text-gray-900"
                 >
                   ZIP / Postal code
                 </label>
@@ -251,9 +318,15 @@ export default function MultiStepForm() {
                   <input
                     type="text"
                     id="zip"
+                    {...register("zip")}
                     autoComplete="postal-code"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                   />
+                  {errors.zip?.message && (
+                    <p className="mt-2 text-sm text-red-400">
+                      {errors.zip.message}
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
